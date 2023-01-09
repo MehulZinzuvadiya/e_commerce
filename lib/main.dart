5be2b58b4cd943459c0cb26bbccd09bbbf7c1bc0
mehul <mehulzinzuvadiya098@gmail.com>
2023-01-09 15:58:27 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:e_commerce/homePage.dart';
+import 'package:flutter/material.dart';
+
+import 'detail.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: E_Commerce(),
+
+  ));
+}
